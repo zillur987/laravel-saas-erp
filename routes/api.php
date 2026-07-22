47e7235b76a63904aae94b75bbeb68c183d51db3
Zillur Rahman <zillurrahman3052@gmail.com>
2026-07-22 21:24:35 +0600

    Initialize project, Jwt added

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::group(['middleware' => 'api'], function () {
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('register', [AuthController::class, 'register']);
+
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::get('me', [AuthController::class, 'me']);
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
+    });
+});
